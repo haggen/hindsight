0dac6ef264f490ea510e53460f72aaf2a5193cd2
Arthur <arthur@corenzan.com>
2024-07-15 09:10:15 -0300

    delete everything

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { ComponentProps } from "react";
-
-export function Button({ ...props }: ComponentProps<"button">) {
-  props.type ??= "button";
-  props.className = `font-bold text-sm ${props.className}`;
-  return <button {...props} />;
-}
--- src/lib/shared/createId.ts
+++
@@ -1,6 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-/**
- * Create fairly unique and URL friendly ID.
- */
-export const createId = customAlphabet("0123456789bcdfghjklmnpqrstvwxyz", 10);
